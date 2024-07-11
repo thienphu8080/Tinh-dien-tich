77a380e0232b1af32ab71afacae11ef46f698359
Nguyen Tran Thien Phu <23110286@student.hcmute.edu.vn>
2024-07-11 16:39:55 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Numerics;
+using System.Text;
+
+namespace Tính_số_lớn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Tính diện tịch hình vuông");
+            Console.Write("Độ dài cạnh: ");
+            BigInteger canh = BigInteger.Parse(Console.ReadLine());
+            Console.WriteLine("Diện tích tam giác {0}", canh * canh);
+        }
+    }
+}
